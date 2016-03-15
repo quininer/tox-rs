adc4a407c9332a476d0d99bdbc51b31b832f0ac5
quininer kel <quininer@live.com>
2016-03-15 23:06:35 +0800

    update(dependencies) ffigen

--- README.md
+++ README.md
@@ -8,18 +8,3 @@ Requirements
 
 * toxcore
 * clang
-
-Build
------
-
-You can use environment variables to specify include path.
-
-fish:
-
-	set -x TOX_INCLUDE_PATH /usr/local/include/tox/
-	set -x CLANG_INCLUDE_PATH /usr/lib/clang/3.4.0/include/
-
-bash:
-
-	export TOX_INCLUDE_PATH=/usr/local/include/tox/
-	export CLANG_INCLUDE_PATH=/usr/lib/clang/3.4.0/include/
--- build.rs
+++ build.rs
@@ -1,39 +1,14 @@
 #![feature(stmt_expr_attributes)]
 
-extern crate ffigen;
-
-use std::env::var;
-use std::fs::File;
-use std::io::Write;
-use ffigen::GenOptions;
-
-const CLANG_INCLUDE_PATH: &'static str = "/usr/lib/clang/3.7.1/include/";
-const TOX_INCLUDE_PATH: &'static str = "/usr/include/tox/";
-
-
-macro_rules! gen {
-    ( $l:expr, [ $( $h:expr ),* ], $o:expr ) => {{
-        let data = GenOptions::default()
-            .arg(&format!("-I{}", var("CLANG_INCLUDE_PATH").unwrap_or(CLANG_INCLUDE_PATH.into())))
-        $(
-            .header(&format!("{}{}", var("TOX_INCLUDE_PATH").unwrap_or(TOX_INCLUDE_PATH.into()), $h))
-        )*
-            .link($l)
-            .gen();
-
-        File::create(format!("src/{}", $o)).unwrap()
-            .write(&data).unwrap();
-    }};
-}
-
+#[macro_use] extern crate ffigen;
 
 fn main() {
     #[cfg(feature = "groupchat")]
-    gen!("toxcore", ["tox.h", "tox_old.h"], "core/ffi.rs");
+    gen!("toxcore", ["tox/tox.h", "tox/tox_old.h"] -> "src/core/ffi.rs");
 
     #[cfg(feature = "newgroupchat")]
-    gen!("toxcore", ["tox.h"], "core/ffi.rs");
+    gen!("toxcore", ["tox/tox.h"] -> "src/core/ffi.rs");
 
-    gen!("toxav", ["toxav.h"], "av/ffi.rs");
-    gen!("toxencryptsave", ["toxencryptsave.h"], "encryptsave/ffi.rs");
+    gen!("toxav", ["tox/toxav.h"] -> "src/av/ffi.rs");
+    gen!("toxencryptsave", ["tox/toxencryptsave.h"] -> "src/encryptsave/ffi.rs");
 }
